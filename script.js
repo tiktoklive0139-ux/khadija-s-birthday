be0ac0c9d4324c0fea56ab33a4b0044e9d720744
tiktoklive0139-ux <tiktoklive0139@gmail.com>
2026-06-26 01:37:45 +0500

    Update script.js

--- script.js
+++ script.js
@@ -27,11 +27,15 @@ function checkPassword() {
 
 /* ================= SLIDESHOW FIXED ================= */
 const photos = [
-  "photos/photo1.jpg",
-  "photos/photo2.jpg",
-  "photos/photo3.jpg",
-  "photos/photo4.jpg",
-  "photos/photo5.jpg"
+  "photo1.jpg",
+  "photo2.jpg",
+  "photo3.jpg",
+  "photo4.jpg",
+  "photo5.jpg",
+  "Photo6.jpg",
+  "photo7.jpg",
+  "photo8.jpg",
+  "photo9.jpg"
 ];
 
 let current = 0;
